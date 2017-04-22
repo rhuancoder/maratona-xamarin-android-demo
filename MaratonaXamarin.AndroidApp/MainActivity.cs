0536675208b17dc9e289bdb5db9cf60b53e2f327
Rhuan S. J. L. Carvalho <rsjlcarvalho@gmail.com>
2017-04-22 15:03:23 -0300

    Add project files.

---
+++ MaratonaXamarin.AndroidApp/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Linq;
+
+namespace MaratonaXamarin.AndroidApp
+{
+    [Activity(Label = "MaratonaXamarin.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var button = this.FindViewById<Button>(Resource.Id.btnCarregar);
+            var listview = this.FindViewById<ListView>(Resource.Id.lvwItens);
+
+            button.Click += async (sender,e) =>
+            {
+                var api = new Shared.UserApi();
+                var users = await api.ListAsync(new Shared.Developer
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Rhuan",
+                    Email = "test@email.com",
+                    State = "RJ",
+                    City = "Rio de Janeiro"
+                });
+
+                listview.Adapter = new ArrayAdapter(this,
+                                                    Android.Resource.Layout.SimpleListItemSingleChoice,
+                                                    users.OrderBy(y => y.Name)
+                                                         .Select(x => $"{x.Id} {x.Name}")
+                                                         .ToArray()
+                                                    );
+            }; 
+            
+        }
+        
+    }
+}
+
---
+++ MaratonaXaramin.Shared/Developer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MaratonaXamarin.Shared
+{
+    public class Developer
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+    }
+}
